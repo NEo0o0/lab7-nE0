5b80dd021bd5ea3c9889f8ec70a86accb1b3800f
NEo0o0 <nathdanai.nepal@gmail.com>
2023-12-12 13:34:52 +0700

    complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,5 +1,44 @@
-//Dialog of the first example is given below.
+#include<iostream>
+using namespace std;
 
+void fahsai(string sent,string sent2,string sent3,string sent4){
+    cout<<"Fahsai: "<<sent<<sent2<<sent3<<sent4<<endl;
+}
+void chat(string urname){
+    cout<<urname<<": ";
+    
+}
+
+int main(){
+    string name="?????",movie,day,x;
+    int gear;
+    fahsai("Sawadee ka...Can you tell me your name?","","","");
+    chat(name);
+    getline(cin,name);
+    fahsai("Wow!!! ",name," is a really cool name.","");
+    fahsai("I think you are an Engineering student. What is your student ID?","","","");
+    chat(name);
+    cin>>gear;
+    cin.ignore();
+    gear=gear/10000000;
+    gear=gear-12;
+    cout<<"Fahsai: I think you may be GEAR "<<gear<<". I have a free movie ticket for you."<<endl;
+    fahsai("Let's go to the cinema together!!!","","","");
+    fahsai("What movie do you want to watch?","","","");
+    chat(name);
+    getline(cin,movie);
+    fahsai("So....which day are you free to go with me?","","","");
+    chat(name);
+    getline(cin,day);
+    fahsai(day,"....that is OK!!! I'm looking forward to watching ",movie," with you.");
+    chat(name);
+    getline(cin,x);
+    fahsai("555+ see you ",day,". Bye Bye \\(^ ^)/","");
+    return 0;
+
+
+
+}
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
 ?????: Luke Skywalker
